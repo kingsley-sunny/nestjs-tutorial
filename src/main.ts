95855d488f431a5ec2683618239338cf52bd2374
kingsley-sunny <ezeobisunny51@gmail.com>
2023-06-09 22:13:19 +0100

    initial commit: make my first nestjs servicer

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Module } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+
+@Controller()
+class AppController {
+  @Get("/")
+  getRootRoute() {
+    return "Hi There";
+  }
+}
+
+@Module({
+  controllers: [AppController],
+})
+class AppModule {}
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(8080);
+}
+
+bootstrap();
