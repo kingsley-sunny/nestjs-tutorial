8b7be761d5417ef0851998d327d095f0f2c5a241
kingsley-sunny <ezeobisunny51@gmail.com>
2023-06-10 11:49:37 +0100

    added dependency injection

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,11 +1,9 @@
+import { Injectable } from '@nestjs/common';
 import MessagesRepository from './messages.repository';
 
+@Injectable()
 class MessagesService {
-  messagesRepo: MessagesRepository;
-
-  constructor() {
-    // Service is creating its own dependencies
-    // DON'T DO THIS ON REAL APPS
+  constructor(private messagesRepo: MessagesRepository) {
     this.messagesRepo = new MessagesRepository();
   }
 
